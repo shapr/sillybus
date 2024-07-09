6b32e8333d0b6934783cb4b8e86ef72daa2d9bb4
Amelia Riely <ameliariely@users.noreply.github.com>
2024-07-09 14:44:09 -0400

    Remove code for html and svg outputs

--- src/main.rs
+++ src/main.rs
@@ -7,31 +7,32 @@ use std::path;
 use std::path::PathBuf;
 
 fn main() {
-    //let output_kind = "html";
-    //let output_kind = "txt";
-    //let output_kind = "svg";
     let file = env::args().nth(1).unwrap();
-    let output_kind = env::args().nth(2).unwrap_or_else(|| "txt".to_owned());
-    println!("{}", file);
-    let path = path::Path::new(&file);
-    let filename = path.file_name().expect("expected a filename");
-    let mut output_file = PathBuf::new();
-    output_file.push(filename);
-    output_file.set_extension(&output_kind);
+    println!("Converting {} to text", file);
+    let input_path = path::Path::new(&file);
+    
+    convert_pdf_to_text(input_path);
+
+    //TODO: print the output path
+    println!("Conversion complete");
+
+}
+
+fn convert_pdf_to_text(input_path: &std::path::Path) {
+    let filename = input_path.file_name().expect("expected a filename");
+    
+    let mut output_path = PathBuf::new();
+    output_path.push("txt");
+    output_path.push(filename);
+    output_path.set_extension("txt");
     let mut output_file =
-	BufWriter::new(File::create(output_file).expect("could not create output"));
-    let doc = Document::load(path).unwrap();
+    BufWriter::new(File::create(output_path).expect("could not create output"));
+    let doc = Document::load(input_path).unwrap();
 
     print_metadata(&doc);
 
-    let mut output: Box<dyn OutputDev> = match output_kind.as_ref() {
-	"txt" => Box::new(PlainTextOutput::new(
-	    &mut output_file as &mut dyn std::io::Write,
-	)),
-	"html" => Box::new(HTMLOutput::new(&mut output_file)),
-	"svg" => Box::new(SVGOutput::new(&mut output_file)),
-	_ => panic!(),
-    };
+    let mut output = PlainTextOutput::new(
+        &mut output_file as &mut dyn std::io::Write);
 
-    output_doc(&doc, output.as_mut()).unwrap();
+    output_doc(&doc, &mut output).unwrap();
 }
